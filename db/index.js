cea23a16fee1e0cd64792379db08435e0e6d847d
Austin Moore <austin1moore@gmail.com>
2022-10-03 18:54:53 -0500

    added questions

--- db/index.js
+++ db/index.js
@@ -10,4 +10,100 @@ findAllEmployees(){
     )
 }
 }
+findAllPossibleManagers(employeeId); {
+    return this.connection.promise().query(
+      "SELECT id, first_name, last_name FROM employee WHERE id != ?",
+      employeeId
+    );
+  }
+
+  // This creates a new employee
+  createEmployee(employee); {
+    return this.connection.promise().query("INSERT INTO employee SET ?", employee);
+  }
+
+  // This removes an employee with the given id
+  removeEmployee(employeeId); {
+    return this.connection.promise().query(
+      "DELETE FROM employee WHERE id = ?",
+      employeeId
+    );
+  }
+
+  // This updates the given employee's role
+  updateEmployeeRole(employeeId, roleId); {
+    return this.connection.promise().query(
+      "UPDATE employee SET role_id = ? WHERE id = ?",
+      [roleId, employeeId]
+    );
+  }
+
+  // Updates the given employee's manager
+  updateEmployeeManager(employeeId, managerId); {
+    return this.connection.promise().query(
+      "UPDATE employee SET manager_id = ? WHERE id = ?",
+      [managerId, employeeId]
+    );
+  }
+
+  // Finds all roles, joins with departments to display the department name
+  findAllRoles(); {
+    return this.connection.promise().query(
+      "SELECT role.id, role.title, department.name AS department, role.salary FROM role LEFT JOIN department on role.department_id = department.id;"
+    );
+  }
+
+  // Creates a new role
+  createRole(role); {
+    return this.connection.promise().query("INSERT INTO role SET ?", role);
+  }
+
+  // Removes a role from the db
+  removeRole(roleId); {
+    return this.connection.promise().query("DELETE FROM role WHERE id = ?", roleId);
+  }
+
+  // Finds all departments
+  findAllDepartments(); {
+    return this.connection.promise().query(
+      "SELECT department.id, department.name FROM department;"
+    );
+  }
+
+  // Finds all departments, join with employees and roles and sum up utilized department budget
+  viewDepartmentBudgets(); {
+    return this.connection.promise().query(
+      "SELECT department.id, department.name, SUM(role.salary) AS utilized_budget FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id GROUP BY department.id, department.name;"
+    );
+  }
+
+  // Creates a new department
+  createDepartment(department); {
+    return this.connection.promise().query("INSERT INTO department SET ?", department);
+  }
+
+  // Removes a department
+  removeDepartment(departmentId); {
+    return this.connection.promise().query(
+      "DELETE FROM department WHERE id = ?",
+      departmentId
+    );
+  }
+
+  // Finds all employees in a given department, join with roles to display role titles
+  findAllEmployeesByDepartment(departmentId); {
+    return this.connection.promise().query(
+      "SELECT employee.id, employee.first_name, employee.last_name, role.title FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department department on role.department_id = department.id WHERE department.id = ?;",
+      departmentId
+    );
+  }
+
+  // Finds all employees by manager, join with departments and roles to display titles and department names
+  findAllEmployeesByManager(managerId); {
+    return this.connection.promise().query(
+      "SELECT employee.id, employee.first_name, employee.last_name, department.name AS department, role.title FROM employee LEFT JOIN role on role.id = employee.role_id LEFT JOIN department ON department.id = role.department_id WHERE manager_id = ?;",
+      managerId
+    );
+  }
+
 module.exports = new DB(connection)
--- db/schema.sql
+++ db/schema.sql
@@ -31,12 +31,12 @@ insert into department(name) values
 ('Accounting');
 
 insert into role(salary, title, department_id) values
-(400.00, 'President','1'),
-(200.00, 'RnD','2'),
-(300.00, 'VP_Sales','3'),
-(100.00, 'HR_Rep','4'),
-(50.00, 'Customer_Service_Rep','5'),
-(200.00, 'Accountant','6');
+(400,000.00, 'President','1'),
+(200,000.00, 'RnD','2'),
+(300,000.00, 'VP_Sales','3'),
+(100,000.00, 'HR_Rep','4'),
+(50,000.00, 'Customer_Service_Rep','5'),
+(200,000.00, 'Accountant','6');
 
 insert  into employee(first_name, last_name, role_id, manager_id)
  values 
